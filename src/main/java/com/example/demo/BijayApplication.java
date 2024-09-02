b0413af8e389b8c7742fc55b2392bd003d43eef9
Bijay Dangal <hello@gmail.com>
2024-09-02 14:38:47 +0545

    Initial Commit

---
+++ src/main/java/com/example/demo/BijayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BijayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BijayApplication.class, args);
+	}
+
+}
